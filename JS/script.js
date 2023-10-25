a3b10aa86307da24182e6e4d2c1b7c8baa8f4a13
IqbalHossain4 <iqbal-hossain@mail.com>
2023-10-25 20:23:12 +0600

    add categories

--- JS/script.js
+++ JS/script.js
@@ -22,3 +22,35 @@ const mobileMenu = document.getElementById("mobileMenu");
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+
+// Slider
+
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const slideWidth = slides[0].offsetWidth;
+
+let currentIndex = 0;
+
+// Function to move the slider to the next set of slides
+function moveSlider(direction) {
+  if (direction === "next") {
+    currentIndex += 4;
+  } else if (direction === "prev") {
+    currentIndex -= 4;
+  }
+
+  if (currentIndex < 0) {
+    currentIndex = 0;
+  } else if (currentIndex > slides.length - 4) {
+    currentIndex = slides.length - 4;
+  }
+
+  slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+// Add event listeners for next and previous buttons
+const nextButton = document.getElementById("nextButton");
+const prevButton = document.getElementById("prevButton");
+
+nextButton.addEventListener("click", () => moveSlider("next"));
+prevButton.addEventListener("click", () => moveSlider("prev"));
